ed7d4d138722b7fc84afaf3aabd6bb385edfba95
Jesús Torres <jmtorres@ull.es>
2015-01-27 16:23:06 +0000

    fix download qt creator installers url

--- cookbooks/qtproject/attributes/default.rb
+++ cookbooks/qtproject/attributes/default.rb
@@ -1,1 +1,1 @@
-default[:qt][:online_installers_url]    = "http://download.qt-project.org/official_releases/online_installers"
+default[:qt][:online_installers_url]    = "http://download.qt.io/official_releases/online_installers"
